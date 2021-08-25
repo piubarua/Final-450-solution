9e3e6c8a88c3780f856bdd43f743841f10142e61
Shree Barua <shree2000b@gmail.com>
2021-08-25 20:56:08 +0530

    recursive

--- Linked List/ReverseLL.cpp
+++ Linked List/ReverseLL.cpp
@@ -24,6 +24,8 @@ Constraints:
 1 <= N <= 104
 */
 
+/*ITERATIVE*/
+
 #include<bits/stdc++.h>
 using namespace std;
 struct Node
@@ -55,3 +57,34 @@ class Solution
     }
     
 };
+
+
+/*RECURSIVE*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+ 
+ 
+    Node* reverse(Node* head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+        // Recursive call
+        Node* rest = reverse(head->next);
+        head->next->next = head;
+        
+        head->next = NULL;
+ 
+        return rest;
+    }
+ 
