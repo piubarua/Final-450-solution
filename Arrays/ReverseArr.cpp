91f3e200c2e410b9dc98c25ff6a5b632344af594
Shree Barua <shree2000b@gmail.com>
2021-08-25 18:06:03 +0530

    change

--- Arrays/ReverseArr.cpp
+++ Arrays/ReverseArr.cpp
@@ -1,21 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-string reverseWord(string str){
-    
-  //Your code here
-  int n=str.length();
-    int start=0;
-    int end=n-1;
-    
-    while(start<end){
-        int temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        start++;
-        end--;
-    }
-    return str;
+string reverseStr(string s)
+{
+  int low=0;
+  int high=s.size()-1;
+  while(low<high){
+    swap(s[low],s[high]);
+    low++;
+    high--;
+  }
+  return s;
 }
-//time complexity O(n)
+
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--){
+    string s;
+    cin>>s;
+    cout<<reverseStr(s)<<endl;
+  }
+  return 0;
+}
+
